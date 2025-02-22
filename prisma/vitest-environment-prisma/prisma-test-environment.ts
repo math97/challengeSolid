216e327a4fe605536f3aa2d0dbd4d0283300945d
Matheus DEV <matheusdev@L-LLQPKP2GFG.local>
2025-02-22 14:05:17 +0000

    test(e2e): applying schemas for test's

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,43 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest/environments'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setting up Prisma environment')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync(`npx prisma migrate deploy`)
+
+    console.log(`Setting up Prisma environment with schema ${schema}`)
+
     return {
-      teardown() {},
+      async teardown() {
+        console.log(`Tearing down Prisma environment with schema ${schema}`)
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
+      },
     }
   },
 }
