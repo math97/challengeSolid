96fb271b064112c1dac94aebf7b8fab72aead4e0
Matheus DEV <matheusdev@MacBookPro.lan>
2025-02-23 18:59:40 +0000

    feat(pet): add getpet route

--- src/http/controllers/pets/pet.routes.ts
+++ src/http/controllers/pets/pet.routes.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify'
 import { registerPet } from './register'
 import { verifyJwt } from '@/http/middleware/verify-jwt'
 import { searchPets } from './searchPets'
+import { getPet } from './getPet'
 
 export async function petRoutes(app: FastifyInstance) {
   app.post('/pet', { onRequest: verifyJwt }, registerPet)
   app.get('/pets', searchPets)
+  app.get('/pet', getPet)
 }
